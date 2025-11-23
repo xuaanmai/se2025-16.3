727cf8b5d08870fc3a90b78773768673873ebad0
PhongNgDuy <152369161+PhongNgDuy@users.noreply.github.com>
2025-11-24 00:33:46 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './app/Filament/**/*.php',
+        './app/Http/Livewire/**/*.php',
+        './vendor/filament/**/*.blade.php',
+        './node_modules/flowbite/**/*.js'
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('flowbite/plugin')
+    ],
+}
