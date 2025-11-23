ce3c07ec810d00a2f83f3c2a9255a180f6cdc9f1
PhongNgDuy <152369161+PhongNgDuy@users.noreply.github.com>
2025-11-24 00:38:46 +0700

    update chema

--- database/schema.sql
+++ database/schema.sql
@@ -17,10 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de données : `helper`
---
-
 -- --------------------------------------------------------
 
 --
@@ -1104,3 +1100,4 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
