39380e69d53faf4b78ef93a5d77384d5683e8d27
PhongNgDuy <152369161+PhongNgDuy@users.noreply.github.com>
2025-12-23 13:52:37 +0700

    Delete resources/js/js directory

--- resources/js/js/bootstrap.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = import.meta.env.VITE_APP_URL || 'http://localhost:8000';
-window.axios.defaults.withCredentials = true;
-
